74683c8ea26e06d8bfbae385dabaaecc746ba8d1
Miguel Hirche <miguelhirchealmeida@gmail.com>
2022-11-28 23:36:46 +0000

    updated working code

--- source/functions.ino
+++ source/functions.ino
@@ -11,29 +11,29 @@
 // range [0, 100], where HIGHEST is the maximum value obtained during the sensor calibration.
 // The mapped value of the level sensor is passed by reference. A boolean variable which indicates
 // if the water level is below a specific threshold is also passed by reference.
-void readWaterLevel(const int waterLevelPin, int threshold, int &waterLevelValue, string &waterLevelState){
+void readWaterLevel(const int waterLevelPin, int threshold, int &waterLevelValue, String &waterLevelState){
     waterLevelValue = map(analogRead(waterLevelPin), 480, 660, 0, 100); // [480, 660] is the range obtained through calibration for the wet sensor
-    if(waterLevelValue < threshold) waterLow = true;
-    else waterLow = false;
+    if(waterLevelValue < threshold) waterLevelState = "LOW";
+    else waterLevelState = "GOOD";
 }
 
 // readSoilMoisture reads the value on the soil moisture sensor (analog pin) and maps the value from range 
 // [0, HIGHEST] to range [0, 100], where HIGHEST is the maximum value obtained during the sensor calibration.
 // The mapped value of the soil moisture sensor is passed by reference. A boolean variable which indicates
 // if the moisture level is below a specific threshold is also passed by reference.
-void readSoilMoisture(const int soilMoisturePin, int threshold, int &soilMoistureValue, string &soilMoistureState){
+void readSoilMoisture(const int soilMoisturePin, int threshold, int &soilMoistureValue, String &soilMoistureState){
     soilMoistureValue = map(analogRead(soilMoisturePin), 0, 1024, 0, 100); // [480, 660] is the range obtained through calibration for the wet sensor
-    if(soilMoistureValue < threshold) moistureLow = true;
-    else moistureLow = false;
+    if(soilMoistureValue < threshold) soilMoistureState = "LOW";
+    else soilMoistureState = "GOOD";
 }
 
 // readTemperature reads the temperature value of the DHT11 sensor, and passes it by reference, as well as
 // the state of the value, according to the parameters tempMin and tempMax. It also takes the DHT object as
 // a pointer to avoid copying the object at every function call (every update cycle).
-void readTemperature(DHT* dht, const int dhtPin, int tempMin, int tempMax, int &tempValue, string &tempState){
-    int readData = *dht.read11(dhtPin);
+void readTemperature(dht* DHT, const int dhtPin, int tempMin, int tempMax, int &tempValue, String &tempState){
+    int readData = DHT->read11(dhtPin);
 
-    tempValue = *dht.temperature;
+    tempValue = DHT->temperature;
 
     if(tempValue < tempMin) tempState = "LOW";
     else if(tempValue > tempMax) tempState = "HIGH";
@@ -43,10 +43,10 @@ void readTemperature(DHT* dht, const int dhtPin, int tempMin, int tempMax, int &
 // readHumidity reads the humidity value of the DHT11 sensor, and passes it by reference, as well as
 // the state of the value, according to the parameters humMin and humMax. It also takes the DHT object as
 // a pointer to avoid copying the object at every function call (every update cycle).
-void readHumidity(DHT* dht, const int dhtPin, int humMin, int humMax, int &humValue, string &humState){
-    int readData = *dht.read11(dhtPin);
+void readHumidity(dht* DHT, const int dhtPin, int humMin, int humMax, int &humValue, String &humState){
+    int readData = DHT->read11(dhtPin);
 
-    humValue = *dht.humidity;
+    humValue = DHT->humidity;
 
     if(humValue < humMin) humState = "LOW";
     else if(humValue > humMax) humState = "HIGH";
@@ -56,15 +56,15 @@ void readHumidity(DHT* dht, const int dhtPin, int humMin, int humMax, int &humVa
 // sendData will send data through the serial port, for the bluetooth module to communicate with the app.
 // The function prints the values and statuses for the temperature, humidity, soil moisture and
 // reservoir level, respectively. Each piece of data (integer or string) is separated by the "|" delimiter.
-void sendData(int tempValue, int humValue, int soilMoistureValue, int waterLevelValue, string tempState,
-string humState, string soilMoistureState, string waterLevelState){
-    Serial.print(tempValue);
+void sendData(int tempValue, int humValue, int soilMoistureValue, int waterLevelValue, String tempState,
+String humState, String soilMoistureState, String waterLevelState){
+    Serial.print(abs(tempValue));
     Serial.print("|");
-    Serial.print(humValue);
+    Serial.print(abs(humValue));
     Serial.print("|");
-    Serial.print(soilMoistureValue);
+    Serial.print(abs(soilMoistureValue));
     Serial.print("|");
-    Serial.print(waterLevelValue);
+    Serial.print(abs(waterLevelValue));
     Serial.print("|");
     Serial.print(tempState);
     Serial.print("|");
--- source/update.ino
+++ source/update.ino
@@ -10,22 +10,24 @@
 // WATER LEVEL SENSOR
 const int waterLevelPin = A0;
 int waterLevelValue;
-string waterLevelState;
+String waterLevelState;
 
 // SOIL MOISTURE SENSOR
 const int soilMoisturePin = A1;
 int soilMoistureValue;
-string soilMoistureState;
+String soilMoistureState;
 
 // DHT11 (TEMPERATURE & HUMIDITY)
 const int dhtPin = 2;
 int tempValue;
 int humValue;
-string tempState;
-string humState;
-DHT* dht;
+String tempState;
+String humState;
+dht* DHT;
 
 // CYCLE
+unsigned long initialTime = 0;
+unsigned long currentTime = 0;
 int updateTime = 10000;
 int numCycles = 0;
 
@@ -38,25 +40,26 @@ void setup(){
 }
 
 void loop(){
-    unsigned long initialTime = millis();
+    currentTime = millis();
 
-    // UPDATE CYCLE
-    while(millis() - initialTime < updateTime){
-        readWaterLevel(waterLevelPin, 35, &waterLevelValue, &waterLevelState);
-        readSoilMoisture(soilMoisturePin, 30, &soilMoistureValue, &soilMoistureState);
-        readTemperature(dht, dhtPin, 18, 30, &tempValue, &tempState);
-        readHumidity(dht, dhtPin, 40, 70, &humValue, &humState);
+    // UPDATE CYCLE 
+    if(currentTime - initialTime >= updateTime){
+        readWaterLevel(waterLevelPin, 35, waterLevelValue, waterLevelState);
+        readSoilMoisture(soilMoisturePin, 30, soilMoistureValue, soilMoistureState);
+        readTemperature(DHT, dhtPin, 18, 30, tempValue, tempState);
+        readHumidity(DHT, dhtPin, 40, 70, humValue, humState);
 
         // SEND DATA TO APP VIA BLUETOOTH
-        sendData(tempValue, humValue, soilMoistureValue, waterLevelValue, tempState, humState, soilMoistureState, waterLevelState)
+        sendData(tempValue, humValue, soilMoistureValue, waterLevelValue, tempState, humState, soilMoistureState, waterLevelState);
 
         // ACTUATE WATER PUMP AFTER AT LEAST 5 MINUTES FROM LAST ACTUATION
-        if(moistureLow && numCycles >= 30){
+        /*if(moistureLow && numCycles >= 30){
 
 
             numCycles = 0;
         }
 
-        numCycles++;
+        numCycles++;*/
+        initialTime = currentTime;
     }
 }
